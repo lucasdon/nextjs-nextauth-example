d6ec4a0ea41c26c46928a5f591856a1422e93aae
lucasdon <15727423+lucasdon@users.noreply.github.com>
2022-12-14 17:28:54 +0100

    add script to head component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import { SessionProvider } from "next-auth/react"
-import "./styles.css"
-import Script from 'next/script'
+import { SessionProvider } from "next-auth/react";
+import "./styles.css";
+import Script from "next/script";
+import Head from "next/head";
 
-import type { AppProps } from "next/app"
-import type { Session } from "next-auth"
+import type { AppProps } from "next/app";
+import type { Session } from "next-auth";
 
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
@@ -12,9 +13,13 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps<{ session: Session }>) {
   return (
-    <SessionProvider session={session}>
-      <script src="https://steadyhq.com/widget_loader/e7341ba7-b314-40d0-8c3c-5dccd8a7a260" />
-      <Component {...pageProps} />
-    </SessionProvider>
-  )
+    <>
+      <Head>
+        <Script src="https://steadyhq.com/widget_loader/e7341ba7-b314-40d0-8c3c-5dccd8a7a260" />
+      </Head>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
+  );
 }
