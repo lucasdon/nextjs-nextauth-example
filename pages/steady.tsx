bf3a473760539de9de13ecf92e0a719f70e38b10
lucasdon <15727423+lucasdon@users.noreply.github.com>
2022-12-14 17:34:38 +0100

    fixing

--- pages/steady.tsx
+++ pages/steady.tsx
@@ -1,12 +1,22 @@
-import Layout from "../components/layout"
+import Layout from "../components/layout";
 
 export default function IndexPage() {
   return (
     <Layout>
       <h1>Steady</h1>
       <p>
-      <div id={"insert_steady_checkout_here"} style={{display: "none"}}></div>.
+        <div
+          id={"insert_steady_checkout_here"}
+          style={{ display: "none" }}
+        ></div>
+        .
+        <a
+          className={"steady-login-button"}
+          data-size={"medium"}
+          data-language={"de"}
+          style={{ display: "none" }}
+        ></a>
       </p>
     </Layout>
-  )
+  );
 }
