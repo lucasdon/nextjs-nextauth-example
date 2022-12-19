fbf9dae28e0bcb7eee395a56ad8adc84dde5bbc3
lucasdon <15727423+lucasdon@users.noreply.github.com>
2022-12-19 17:39:00 +0100

    test

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -57,6 +57,14 @@ export const authOptions: NextAuthOptions = {
       token.userRole = "admin";
       return token;
     },
+    async redirect({ url, baseUrl }) {
+      console.log("redirect", url, baseUrl);
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url
+      return baseUrl
+    }
   },
 };
 
